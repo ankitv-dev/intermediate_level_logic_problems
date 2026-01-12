ef328433f16634f5a1761b38102257bf5897721e
ankitv-dev <ankit.kumar.verma.dev@gmail.com>
2026-01-12 19:22:13 +0530

    Done Q2

--- 02_problem.rs
+++ 02_problem.rs
@@ -7,5 +7,20 @@ case. Return the total count.
 *Output:* `4`
 */
 
+fn main() {
+    let st = "Rust";
+    println!("{}", vowel_count(st))
+}
 
+fn vowel_count(s:&str)->i32{
+    let mut count = 0;
 
+    for ch in s.chars() {
+        match ch.to_ascii_lowercase() {
+            'a' | 'e' | 'i' | 'o' | 'u' => count += 1,
+            _ => {}
+        }
+    }
+
+    count
+}
